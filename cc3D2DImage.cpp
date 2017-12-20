558c530fd58e7a444c0bbba56bccd7b56b988592
Iris <irisdegelis@gmail.com>
2017-12-20 15:52:26 +0100

    Creation classe cc3D2DImage

---
+++ cc3D2DImage.cpp
@@ -0,0 +1,7 @@
+#include "cc3D2DImage.h"
+
+cc3D2DImage::cc3D2DImage()
+{
+
+}
+
---
+++ cc3D2DImage.h
@@ -0,0 +1,11 @@
+#ifndef CC3D2DIMAGE_H
+#define CC3D2DIMAGE_H
+
+
+class cc3D2DImage
+{
+public:
+    cc3D2DImage();
+};
+
+#endif // CC3D2DIMAGE_H
